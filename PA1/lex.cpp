6e68aa18250b9371af989a195098d7f5c3f46e58
unknown <hallsamir14@gmail.com>
2023-10-09 12:41:45 -0400

    Updated test cases and lex header mods

---
+++ PA1/lex.cpp
@@ -0,0 +1,4 @@
+// Lexical analyzer function --> getNextToken(istream& in, int& linenumber)
+//First argument is reernce to istream object
+//Second reference is integer that stores current line number of line read
+//getNextToken returns LexItem object
--- PA1/lex.h
+++ PA1/lex.h
@@ -1,3 +1,12 @@
+/*
+LexItem class contains token, string for lexeme, and line number
+as data members
+*/
+
+
+
+
+
 //Definition of all the possible token types
 enum Token {
 // keywords OR RESERVED WORDS
@@ -19,27 +28,31 @@ DONE,
 };
 
 class LexItem { //Class definition of LexItem
-Token token;
-string lexeme;
-int lnum;
+    Token token;
+    string lexeme;
+    int lnum;
+
 public:
-LexItem() {
-token = ERR;
-lnum = -1;
-}
-LexItem(Token token, string lexeme, int line) {
-this->token = token;
-this->lexeme = lexeme;
-this->lnum = line;
-}
+    LexItem() {
+    token = ERR;
+    lnum = -1;
+    }
+    LexItem(Token token, string lexeme, int line) {
+    this->token = token;
+    this->lexeme = lexeme;
+    this->lnum = line;
+    }
+
 bool operator==(const Token token) const { return this->token ==
 token; }
 bool operator!=(const Token token) const { return this->token !=
 token; }
+
 Token GetToken() const { return token; }
 string GetLexeme() const { return lexeme; }
 int GetLinenum() const { return lnum; }
 };
+
 bool operator==(const Token token) const {
 return this->token == token; }
 bool operator!=(const Token token) const {
